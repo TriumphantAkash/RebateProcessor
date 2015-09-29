a58b2d33069a479610cfeb52d6efa904ed91f6ea
Akash Chaturvedi <axc144430@utdallas.edu>
2015-09-28 22:34:17 -0500

    deleteData() function in FileHandler

--- src/rebateprocessing/FileHandler.java
+++ src/rebateprocessing/FileHandler.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 class FileHandler {
    
     private static final String FILE_NAME = "rebate_processing_data_file.txt";
+    private static final String TEMP_FILE = "temp420.txt";
     
     
     //data object is passed to this function, it writes the data to file
@@ -115,6 +116,73 @@ class FileHandler {
         
         return appData;
     }
+   
+   //remove the entry from file that contains the name specified
+   /*   I am gonna do this
+   
+        * Open the old file for reading
+        * Open a new (temporary) file for writing
+        * Iterate over the lines in the old file (probably using a BufferedReader)
+        * For each line, check if it matches what you are supposed to remove
+        * If it matches, do nothing
+        * If it doesn't match, write it to the temporary file
+        * When done, close both files
+        * Delete the old file
+        * Rename the temporary file to the name of the original file
+   
+    */
+   
+   public static boolean deleteData (String fullName) {
+       try {
+           
+           FileReader fileReader = 
+                new FileReader(FILE_NAME);
+                
+           // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+           
+           /********************************/
+            // Assume default encoding.
+            FileWriter fileWriter =
+                new FileWriter(TEMP_FILE, true);
+
+            // Always wrap FileWriter in BufferedWriter.
+            BufferedWriter bufferedWriter =
+                new BufferedWriter(fileWriter);
+            
+            
+            /*********************************/
+              String line;
+            
+            while((line = bufferedReader.readLine()) != null) {
+                if(line.startsWith(fullName)){  //don't write this line to new file
+                }else{ //write line to this new file
+                    bufferedWriter.write(line);
+                }
+            }
+            
+            /**********************************/
+            
+            
+           
+       }catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                FILE_NAME + "'"); 
+            return false;
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + FILE_NAME + "'");
+            // Or we could just do this: 
+            // ex.printStackTrace();
+            return false;
+        }
+       
+       return true;
+   }
     
     boolean modifyData(String fullName) {
         //modify the data based on the parameter passed
--- src/rebateprocessing/RebateProcessingController.java
+++ src/rebateprocessing/RebateProcessingController.java
@@ -29,6 +29,8 @@ public class RebateProcessingController {
      
      
  }
+ 
+ //boolean deleteData()
 
    
     
